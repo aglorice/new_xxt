327653456b946a6156d6e1fc3129bf09f6239fca
aglorice <401208941@qq.com>
2023-05-24 16:42:04 +0800

    refactor:🪲

--- main.py
+++ main.py
@@ -1,53 +1,15 @@
-from xcxy_xxt import XcxyXxt, batchWork
+from rich.console import Console
 
-# 已完成该作业的账号
-phone = ""
-password = ""
 
-# 未完成作业的账号和密码
-_phone = ""
-_password = ""
+from my_xxt.my_tools import select_menu, show_start
+from my_xxt.api import XcxyXxt
+from my_xxt.login import login
 
-# 学校的学习通id
-fid = 2397  # 选填
-refer = "http://scxcc.fanya.chaoxing.com/login/auth"  # 选填
-
-work_name = "第3章 堆栈和队列-作业"
-course_name = "数据结构与算法"
+console = Console(width=100)
 
 if __name__ == '__main__':
-    # """查询所有未作的作业，并在已完成的答案中提取正确答案并保存在answer文件中"""
-    # user_1 = XcxyXxt(phone=phone, password=password, fid=fid, refer=refer)
-    # user_2 = XcxyXxt(phone=_phone, password=_password, fid=fid, refer=refer)
-    # user_1.Login()
-    # user_1.getCourseDate()
-    # user_2.Login()
-    # user_2.getCourseDate()
-    # work_list = user_2.getAllNotDoneWork()  # 获取所有课程未作的作业
-    # user_1.baseNotDoneWorkGetAnswer(work_list)  # 根据未做的作业来寻找正确的答案
-
-    """查询指定的作业并完成它"""
-    print("<<<<======================================================>>>>")
-    user_1 = XcxyXxt(phone=phone, password=password, fid=fid, refer=refer)
-    user_1.Login()
-    user_1.getCourseDate()
-    user_1.getCourseWork(course_name)
-    answer = user_1.getWorkAnswer(work_name)
-    user_1.dateToJsonFile("answer.json", answer)
-    print("<<<<======================================================>>>>\n\n\n")
-
-    print("<<<<======================================================>>>>")
-    user_2 = XcxyXxt(phone=_phone, password=_password, fid=fid, refer=refer)
-    user_2.Login()
-    user_2.getCourseDate()
-    user_2.getCourseWork(course_name)
-    user_2.getWorkView(work_name, "answer.json")
-    user_2.completedWork(work_name=work_name)
-    print("<<<<======================================================>>>>")
-
-    # """批量完成作业"""
-    # print("<<<<======================================================>>>>")
-    #
-    # batchWork(work_name=work_name, course_name=course_name, fid=fid, refer=refer, file='user.json')
-    #
-    # print("<<<<======================================================>>>>")
+    while True:
+        show_start(console)
+        xxt = XcxyXxt()
+        login(console, xxt)
+        select_menu(console, xxt)
---
+++ my_xxt/login.py
@@ -0,0 +1,23 @@
+# -*- coding = utf-8 -*-
+# @Time :2023/5/16 20:05
+# @Author :小岳
+# @Email  :401208941@qq.com
+# @PROJECT_NAME :xxt_cli
+# @File :  login.py
+from rich.console import Console
+
+from my_xxt.api import XcxyXxt
+
+
+def login(console: Console, xxt: XcxyXxt) -> None:
+    while True:
+        phone = console.input("[yellow]请输入手机号:")
+        password = console.input("[yellow]请输入密码:")
+        login_status = xxt.login(phone, password)
+        if not login_status["status"]:
+            flag = console.input("[green]登录失败,请重新尝试！(按q退出,按任意键继续)")
+            if flag == 'q':
+                exit()
+        else:
+            console.print("[green]登录成功")
+            break
